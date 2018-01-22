513f032485286c63bfe6ccdf7773534351e35061
Lori <lori.tieshue@gmail.com>
2018-01-22 16:07:29 -0700

    Third Commit 4:00PM Monday

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,31 +22,39 @@ memberInfo cust9 = new memberInfo("901", "Jake", 0.0);
 memberInfo cust10 = new memberInfo("012", "Scott", 0.0);
 
 
-
-
-            cust1.deposit(57000);
-            cust2.deposit(57000);
-            cust3.deposit(57000);
-            cust4.deposit(57000);
-            cust5.deposit(57000);
-            cust6.deposit(57000);
-            cust7.deposit(57000);
-            cust8.deposit(57000);
-            cust9.deposit(57000);
-            cust10.deposit(57000);
+memberInfo[] listofAccounts = {cust1, cust2, cust3, cust4, cust5, cust6, cust7, cust8, cust9, cust10};
+            for (int i = 0; i<listofAccounts.length; i++){                    listofAccounts[i].deposit(57000);
+                    listofAccounts[i].withdrawal(7000);
+                    System.out.println(listofAccounts[i].memberName + "'s Account # is " + listofAccounts[i].accountNumber + ". Account Balance is: $" + listofAccounts[i].getBalance());}
 
 
 
-            System.out.println(cust1);
-            System.out.println(cust2);
-            System.out.println(cust3);
-            System.out.println(cust4);
-            System.out.println(cust5);
-            System.out.println(cust6);
-            System.out.println(cust7);
-            System.out.println(cust8);
-            System.out.println(cust9);
-            System.out.println(cust10);
-        }
+//            cust1.deposit(57000);
+//            cust2.deposit(57000);
+//            cust3.deposit(57000);
+//            cust4.deposit(57000);
+//            cust5.deposit(57000);
+//            cust6.deposit(57000);
+//            cust7.deposit(57000);
+//            cust8.deposit(57000);
+//            cust9.deposit(57000);
+//            cust10.deposit(57000);
+//
+
+
+
+
+//            System.out.println(cust1);
+//            System.out.println(cust2);
+//            System.out.println(cust3);
+//            System.out.println(cust4);
+//            System.out.println(cust5);
+//            System.out.println(cust6);
+//            System.out.println(cust7);
+//            System.out.println(cust8);
+//            System.out.println(cust9);
+//            System.out.println(cust10);
+
+                   }
     }
 
